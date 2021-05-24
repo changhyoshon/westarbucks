f67663d4ac6c177e014b671d4f76ce486e3dd058
손창효 <ckdgy123@gmail.com>
2021-05-24 17:06:29 +0900

    CREATE: 프로젝트 초기 셋팅

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks',
+        'USER': 'root',
+        'PASSWORD': 'Vs5453vs!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-q+^#40*sv-1sp!(p0)f1&&q%2$q^(s%xoizzs_s!$*!91q7nv)'
